27ea819fdb795a8334d1a4d4b119b24a1424d87f
mrpindro <118183486+mrpindro@users.noreply.github.com>
2023-10-08 17:10:25 +0200

    addings

--- src/api/apiSlice.js
+++ src/api/apiSlice.js
@@ -2,8 +2,8 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { setCredentials } from '../features/auth/authSlice';
 
 const baseQuery = fetchBaseQuery({
-    baseUrl: 'http://localhost:5500',
-    // baseUrl: 'https://pindroblog-api.onrender.com',
+    // baseUrl: 'http://localhost:5500',
+    baseUrl: 'https://pindroblog-api.onrender.com',
     credentials: 'include',
     prepareHeaders: (headers, { getState }) => {
         const token = getState().auth.token;
